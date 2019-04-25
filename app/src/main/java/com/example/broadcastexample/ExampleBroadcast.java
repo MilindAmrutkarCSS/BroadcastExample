88ede3bd78bd1cfa0166e89987da79521eaeaef2
Milind Amrutkar <cssindiamilind@gmail.com>
2019-04-25 11:27:29 +0530

    Implemented our Explicit Broadcast

--- app/src/main/java/com/example/broadcastexample/ExampleBroadcast.java
+++ app/src/main/java/com/example/broadcastexample/ExampleBroadcast.java
@@ -18,15 +18,11 @@ public class ExampleBroadcast extends BroadcastReceiver {
      *
      *  Or we can send an implicit broadcast which is
      *  basically the same as the System broadcast
-     *
      */
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if ("com.example.EXAMPLE_ACTION".equals(intent.getAction())) {
-            String receivedText = intent.getStringExtra("com.example.EXTRA_TEXT");
-            Toast.makeText(context, receivedText, Toast.LENGTH_SHORT).show();
-        }
+        Toast.makeText(context, "EBR triggered", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/broadcastexample/MainActivity.java
+++ app/src/main/java/com/example/broadcastexample/MainActivity.java
@@ -1,34 +1,13 @@
 package com.example.broadcastexample;
 
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ExampleBroadcast exampleBroadcast = new ExampleBroadcast();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //We filter our request and register the receiver
-        IntentFilter filter = new IntentFilter("com.example.EXAMPLE_ACTION");
-        registerReceiver(exampleBroadcast, filter);
-
-        /**
-         * Create new Android studio project for sending broadcast messages,
-         * so when that app sends a Broadcast message it is received by this
-         * app as a toast message.
-         * And when this app is in background then the app doesn't show toast
-         * message
-         */
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(exampleBroadcast);
     }
 }
